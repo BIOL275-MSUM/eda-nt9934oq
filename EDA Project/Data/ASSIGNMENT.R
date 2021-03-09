c04135e6e0ca83c40f587043e1239dfecf9aaea5
Michael Slaughter <nt9934oq@go.minnstate.edu>
2021-03-09 13:13:07 -0600

    Commit

---
+++ EDA Project/Data/ASSIGNMENT.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+#Loading in Sea Ice Data
+
+read_fwf(
+  "Data/Antarctic Sea Ice Data Set.txt", 
+  fwf_widths(c(6, 10, 12,10,10,10,10,10,10,10,10,10,10), c("Year",
+                                               "Jan",
+                                               "Feb",
+                                               "Mar",
+                                               "Apr",
+                                               "May",
+                                               "June",
+                                               "July",
+                                               "Aug",
+                                               "Sept",
+                                               "Oct",
+                                               "Nov",
+                                               "Dec")),
+  skip = 8
+)
