0a267ae42789e7c01a5df956d04684326c9ddad9
Michael Slaughter <nt9934oq@go.minnstate.edu>
2021-04-06 13:14:35 -0500

    Yes

--- ASSIGNMENT.R
+++ ASSIGNMENT.R
@@ -3,27 +3,33 @@ library(tidyverse)
 #Loading in Sea Ice Data
 seaiceData<-
 read_fwf(
-  "Data/Antarctic Sea Ice Data Set.txt", 
-  fwf_widths(c(6, 10, 12,10,10,10,10,10,10,10,10,10,10), c("Year",
-                                               "Jan",
-                                               "Feb",
-                                               "Mar",
-                                               "Apr",
-                                               "May",
-                                               "June",
-                                               "July",
-                                               "Aug",
-                                               "Sept",
-                                               "Oct",
-                                               "Nov",
-                                               "Dec")),
+  file =  "Data/Antarctic Sea Ice Data Set.txt",
+  col_positions = fwf_widths(
+    widths = c(6, 10, 12,10,10,10,10,10,10,10,10,10,10),
+    col_names = c("Year", "01", "02","03","04","05","06","07","08","09","10","11","12")
+  ),
+  na = "-999.00",
   skip = 8
 )
 
 seaiceData
 
-mutate(text_date = paste0(2001, "-", Jan, "-01")
-       mutate(date = lubridate::as.Date(text_date)
+extendiceData <-
+pivot_longer(
+  data = seaiceData,
+  cols = `01`:`12`,
+  names_to = "Month", 
+  values_to = "Concentration",
+  values_drop_na = TRUE
+  )
+
+extendiceData
+
+mutate(
+extendiceData,
+  text_date = paste0(Year, "-", Month, "-01"),
+  date = as.Date(text_date)
+)
 
               
        
